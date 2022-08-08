40463044b81f93a14c0e4278254d12b240535c19
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-08-08 09:44:26 +0000

    Added isPalindromes function at script.js

--- js/script.js
+++ js/script.js
@@ -8,10 +8,21 @@ function getNumberInfo() {
     if (isNaN(num) || num.toString().length == 0 || num<1000 || num>1000000 || !Number.isInteger(Number(num))) {
         txt += `Invalid Input.  Please enter a whole number between 1000 and 1000000.`;
     } else {
+        // Remove any leading zeros
+        num = parseInt(num);
         txt += `You have entered the number ${num}. <p>`;
         txt += `Sum of all numbers, less then ${num}, which are palindromic in base 10 and base 2 is ${doubleBasePalindromes(num)}`;
     }
 
     // Display Information in the Browser
     document.getElementById("numinfo").innerHTML = txt;
+}
+
+// Function to check if the number n is a palindrome
+function isPalindromes(n) {
+    const nString = n.toString();
+    for (let i=0;i<nString.length / 2;i++) {
+        if (nString[i] !== nString[nString.length - i - 1]) return false;
+    }
+    return true;
 }
