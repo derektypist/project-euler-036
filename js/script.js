3401bc7915f6a8885b92abd4174b994073f0da93
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-08-08 09:53:01 +0000

    Added clearInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -45,3 +45,9 @@ function doubleBasePalindromes(n) {
     }
     return sum;
 }
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById("numinfo").innerHTML = txt;
+}
